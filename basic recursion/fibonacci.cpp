0e3e37f34f4b0bbdecb4ec0bc4242c13bee5ee8d
DishaDewangan <dishadewangan0109@gmail.com>
2025-02-21 22:49:43 +0530

    Daily commit: 2025-02-21

---
+++ basic recursion/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+int main() {
+    Solution sol;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Fibonacci(" << n << ") = " << sol.fib(n) << endl;
+    return 0;
+}
